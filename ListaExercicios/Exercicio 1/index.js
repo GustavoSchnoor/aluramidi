ff2f98a171a1085141f07e3e4907f4962595a681
GustavoSchnoor <153643888+GustavoSchnoor@users.noreply.github.com>
2024-04-10 23:42:40 -0300

    att exercicio 1

--- ListaExercicios/Exercicio 1/index.js
+++ ListaExercicios/Exercicio 1/index.js
@@ -4,9 +4,11 @@ botaoPom.addEventListener('click', function() {
     alert('Botão Pom foi clicado!');
 })
 
-let music1 = document.querySelector('.controlePom');
-let play = document.querySelector('.botao_play');
-let pause = document.querySelector('.botao_pause');
+const music1 = document.querySelector('.controlePom');
+const play = document.querySelector('.botao_play');
+const pause = document.querySelector('.botao_pause');
+const reset = document.querySelector('.botao_reset');
+const controleVolume = document.querySelector('#volume');
 
 play.addEventListener('click', function() {
     music1.play();
@@ -14,4 +16,12 @@ play.addEventListener('click', function() {
 
 pause.addEventListener('click', function() {
     music1.pause();
+})
+
+reset.addEventListener('click', function() {
+    music1.currentTime = 0;
+})
+
+controleVolume.addEventListener('input', function() {
+    music1.volume = controleVolume.value;
 })
