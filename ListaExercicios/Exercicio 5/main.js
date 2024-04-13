f11964caba3653305d393aeaf3b9cff0148e912f
GustavoSchnoor <153643888+GustavoSchnoor@users.noreply.github.com>
2024-04-13 11:33:06 -0300

    Att exercicio 5

--- ListaExercicios/Exercicio 5/main.js
+++ ListaExercicios/Exercicio 5/main.js
@@ -1,53 +1,21 @@
-// 1) Incrementando o contador no console
-
+// 1) Adicionando classe com clique
+// 2) Removendo classe com clique
 /*
 const botoes = document.querySelectorAll('.tecla');
 
-let vezesClicado = 0
-
-for (contador = 0; contador < botoes.length; contador++) {
-    botoes[contador].addEventListener('click', function() {
-        vezesClicado += 1;
-        console.log(`O botão foi clicado ${vezesClicado} vezes`);
-    });
-}
-*/
-
-// 2) Criando um array de botões
-
-const todosBotoes = document.querySelectorAll('.tecla');
-console.log(todosBotoes);
-
-
-// 3) Trabalhando com arrays para incrementar um contador APENAS QUANDO O QUARTO ELEMENTO FOR CLICADO (PUFF)
-
-/*
-let titulo = document.querySelector('h1');
-
-let contador = 0
-todosBotoes[3].addEventListener('click', function() {
-    contador += 1;
-    titulo.innerHTML = `Voce clicou no botão ${contador} vezes!`;
-    console.log(contador);
-})
-*/
-
-// 4) Utilizando método while para aplicar contador
-
-/*
-let contador = 0;
-let vezesClicado = 0;
-
-while (contador < todosBotoes.length) {
-    todosBotoes[contador].addEventListener('click', function() {
-        vezesClicado += 1;
-        console.log(`O total de vezes clicado em qualquer botão, foram de ${vezesClicado} vezes`);
+for (let contador = 0; contador < botoes.length; contador++) {
+    let botao = botoes[contador];
+    botao.addEventListener('click', function() {
+        // com o classList.toggle('ativa') ele vai adicionar caso não tenha, e remover caso tenha. ELIMINANDO a necessidade de fazer mais código manual de checagem.
+        botao.classList.toggle('ativa'); 
     })
-
-    contador++
 }
 */
 
-// 5) Utilizando método while para aplicar contador
+const botoes = document.querySelectorAll('.tecla');
 
-// ( ACABEI FAZENDO A 1 DESSE JEITO, ENTÃO A 5 E A 1 SÃO A MSM COISA)
+botoes.forEach(function(botao) {
+    botao.addEventListener('click', function () {
+        botao.classList.toggle('ativa');
+    })
+})
